d9050c21a9a00c4bef7f18fc2663490ab6a83149
leduytienyl2003 <leduytienyl2003>
2024-10-22 23:56:16 +0700

    Lession27

---
+++ lession27/lab6-1/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6_1
+{
+    internal class Product
+    {
+        //khai báp các trường
+        string name;
+        double cost;
+        int onhand;
+        //constructor
+        public Product(string n, double c, int h)
+        {
+            name = n;
+            cost = c;
+            onhand = h;
+        }
+        //ghi đè phương thức
+        public override string ToString()
+        {
+            return string.Format("{0,-10}Cost: {1,6:C} On hand: {2}", name, cost, onhand);
+        }
+    }
+}
---
+++ lession27/lab6-1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+namespace lab6_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //tạo ArrayList
+            ArrayList inv = new ArrayList();
+            //thêm phầ tử vào danh sách
+            inv.Add(new Product("A", 5.9, 3));
+            inv.Add(new Product("B", 8.2, 2));
+            inv.Add(new Product("C", 3.5, 4));
+            inv.Add(new Product("D", 1.8, 8));
+            //in ra danh sách phần tử
+            Console.WriteLine("Product list:");
+            foreach(Product i in inv)
+            {
+                Console.WriteLine("   "+i);
+            }
+        }
+    }
+}
